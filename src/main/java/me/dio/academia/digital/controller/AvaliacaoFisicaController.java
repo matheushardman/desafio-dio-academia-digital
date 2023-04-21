455484108795eef581baab43d671b6ba06d21119
matheushardman <matheus.hardman@gmail.com>
2023-04-21 13:36:14 -0300

    AvaliacaoFisicaServiceImpl, AvaliacaoFisicaController e AvaliacaoFisicaRepository

---
+++ src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -0,0 +1,23 @@
+package me.dio.academia.digital.controller;
+
+
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.service.impl.AvaliacaoFisicaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoFisicaController {
+
+    @Autowired
+    private AvaliacaoFisicaServiceImpl service;
+
+    @PostMapping
+    public AvaliacaoFisica create(@Valid @RequestBody AvaliacaoFisicaForm form){
+        return service.create(form);
+    }
+}
