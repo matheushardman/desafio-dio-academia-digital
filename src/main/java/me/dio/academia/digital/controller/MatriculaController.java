c12f5b89dc24a8c1fb7181da70c0fd2da1dcd5fe
matheushardman <matheus.hardman@gmail.com>
2023-04-21 15:02:14 -0300

    Criação do método findByAlunoBairro

--- src/main/java/me/dio/academia/digital/controller/MatriculaController.java
+++ src/main/java/me/dio/academia/digital/controller/MatriculaController.java
@@ -4,15 +4,13 @@ import me.dio.academia.digital.entity.Matricula;
 import me.dio.academia.digital.entity.form.MatriculaForm;
 import me.dio.academia.digital.service.impl.MatriculaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping("/matricula")
+@RequestMapping("/matriculas")
 public class MatriculaController {
 
     @Autowired
@@ -22,6 +20,10 @@ public class MatriculaController {
     public Matricula create(@Valid @RequestBody MatriculaForm form){
         return service.create(form);
     }
+
+    @GetMapping
+    public List<Matricula> getAll(@RequestParam(value = "bairro",required = false) String bairro) {
+        return service.getAll(bairro); }
 
 
 }
--- src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
@@ -2,6 +2,11 @@ package me.dio.academia.digital.repository;
 
 import me.dio.academia.digital.entity.Matricula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+    List<Matricula> findByAlunoBairro(String bairro);
 }
